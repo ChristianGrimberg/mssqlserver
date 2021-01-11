b61f958e290b7e61692a2b191720d4b79d770e16
Christian Grimberg <christian.grimberg@outlook.com>
2021-01-11 11:19:45 -0300

    :wrench: Change the seconds to see the SQL Server engine log file.

--- scripts/start-sql-engine.sh
+++ scripts/start-sql-engine.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-Wait=100
+Wait=90
 i=0
-echo Waiting to $Wait seconds to starting SQL Server Engine...
+echo Waiting to starting SQL Server Engine...
+sleep 1
 while [ $i -lt $Wait ];
 do
     tail -n 1 /var/opt/mssql/log/errorlog
